04d4f0ed103e3664d0a4ec9e849a41285a1f1c5d
KONATHALA YASWANTH <132152566+yash-io@users.noreply.github.com>
2024-08-22 18:55:56 +0530

    Update more-tools.jsx

--- src/more-tools.jsx
+++ src/more-tools.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import QR_GEN from "./qr-gen";
+import ImageTextExtractor from "./text"; 
 
+const More_tools = () => {
+  const [selectedTool, setSelectedTool] = useState(null);
 
-const More_tools = () =>{
-    return(
-        <>
-        <QR_GEN />
-        </>
-    );
-}
+  return (
+    <div>
+      <div>
+        <button onClick={() => setSelectedTool("QR")}>QR Tools</button>
+        <button onClick={() => setSelectedTool("OCR")}>OCR Text</button>
+      </div>
+      
+      <div>
+        {selectedTool === "QR" && <QR_GEN />}
+        {selectedTool === "OCR" && <ImageTextExtractor />}
+      </div>
+    </div>
+  );
+};
 
-export default More_tools
+export default More_tools;
